b98faaac3f1f249c4c567b01ca7daa49017f3832
lijun <jun.jeffrey.li@gmail.com>
2021-08-09 15:57:06 +0800

    Shelve javax.xml.bind version conflict - http://localhost:8080/gemis/get/{ID} will break!

--- Gemis/src/main/java/me/takara/gemis/RestfulController.java
+++ Gemis/src/main/java/me/takara/gemis/RestfulController.java
@@ -1,7 +1,6 @@
 package me.takara.gemis;
 
-import jersey.repackaged.com.google.common.base.Stopwatch;
-import me.takara.shared.SyncStamp;
+import com.google.common.base.Stopwatch;
 import me.takara.shared.entities.Bond;
 
 import javax.ws.rs.*;
@@ -13,14 +12,15 @@ import java.util.logging.Logger;
 public class RestfulController {
 
     private static Logger log = Logger.getLogger(RestfulController.class.getName());
-    private String logTime = "Took %s";
+    private String logTime = "Took %,ds";
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String getHello() {
+    public String getHello() throws InterruptedException {
 
-        final Stopwatch sw = new Stopwatch().start();
+        final Stopwatch sw = Stopwatch.createStarted();
+        Thread.sleep(1000L);
         sw.stop();
         log.info(String.format(logTime, sw.elapsed(TimeUnit.MILLISECONDS)));
         return "HELLO! I'm your Gemis.";
@@ -32,7 +32,7 @@ public class RestfulController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Bond getById(@PathParam("id") long id) {
 
-        final Stopwatch sw = new Stopwatch().start();
+        final Stopwatch sw = Stopwatch.createStarted();
 
         Bond obj = Gemis.getInstance().get(id);
 
--- Shared/src/main/java/me/takara/shared/entities/Bond.java
+++ Shared/src/main/java/me/takara/shared/entities/Bond.java
@@ -3,6 +3,8 @@ package me.takara.shared.entities;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+//import javax.xml.bind.annotation.XmlRootElement;
+
 //@XmlRootElement(name = "bond")
 public class Bond {
 
